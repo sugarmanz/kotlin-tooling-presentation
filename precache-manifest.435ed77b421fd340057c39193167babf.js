a7ceba458ad4aed623066f6f3a04110eb20b218a
Jeremiah Zucker <zucker.jeremiah@gmail.com>
2021-05-19 11:16:00 -0700

    3f2b2a2

---
+++ precache-manifest.435ed77b421fd340057c39193167babf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0bc416da4a3dd0c836e850e5c79a75a1",
+    "url": "/kotlin-tooling-presentation/index.html"
+  },
+  {
+    "revision": "2aa4c9e65cb661288e96",
+    "url": "/kotlin-tooling-presentation/static/css/main.3451aed5.chunk.css"
+  },
+  {
+    "revision": "6b85683f17a023673736",
+    "url": "/kotlin-tooling-presentation/static/js/2.0c327fb0.chunk.js"
+  },
+  {
+    "revision": "f74a6debb7da89320918198b818026f8",
+    "url": "/kotlin-tooling-presentation/static/js/2.0c327fb0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2aa4c9e65cb661288e96",
+    "url": "/kotlin-tooling-presentation/static/js/main.196efb5d.chunk.js"
+  },
+  {
+    "revision": "118ebf18ea31f1fd6ce6",
+    "url": "/kotlin-tooling-presentation/static/js/runtime-main.8e1a3780.js"
+  }
+]);
